612f4a79f4ebb33c2dd77778050e6eb0290139da
asejdina <asejdina@gmail.com>
2016-08-11 19:54:14 -0400

    added a new timeline and made it so the navbar looks pretty sharp on the scroll

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function() {
+
+  $(document).ready(init);
+
+  function init(){
+    $('div.hidden').fadeIn(2000).removeClass('hidden');
+
+    $(document).on('scroll', checkNavPos);
+
+    $(window).scroll(function(){
+    	$('.container p').each(function(){
+        	var scrollTop     = $(window).scrollTop(),
+            	elementOffset = $(this).offset().top,
+           		distance      = (elementOffset - scrollTop),
+    			    windowHeight  = $(window).height(),
+    			    breakPoint    = windowHeight*0.9;
+
+    			if(distance > breakPoint) {
+    				$(this).addClass("more-padding");
+    			}  if(distance < breakPoint) {
+    				$(this).removeClass("more-padding");
+    			}
+    	});
+    });
+
+  }
+
+
+  function checkNavPos(){
+    var scrollPos = $('title').position();
+    console.log(scrollPos.top);
+
+    if(scrollPos.top > 50){
+      $('#programHeader').css('background-color', 'white');
+    } else {
+      $('#programHeader').css('background-color', 'transparent');
+    }
+  }
+
+
+
+
+
+});
